69aa9bf5623227df20807b17abb657904d294f09
Letticia Luize Ferreira Ribeiro Santos <lluize@infisconsultoria.com.br>
2023-05-08 10:32:39 -0300

    first commit

---
+++ UsuariosApp.API/Controllers/UsuariosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        /// <summary>
+        /// Autenticação de usuários
+        /// </summary>
+        /// <returns></returns>
+       
+        [HttpPost]
+        [Route("autenticar")]
+        public IActionResult Autenticar()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Criação de conta de usuários
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("criar-conta")]
+        public IActionResult CriarConta()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ UsuariosApp.API/Program.cs
@@ -0,0 +1,16 @@
+using UsuariosApp.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddSwaggerDoc();
+
+var app = builder.Build();
+
+app.UseSwaggerDoc();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
